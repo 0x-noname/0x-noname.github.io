3647d68690a0e7eb947013f8d77fa6e858b1a388
noname <88216638+0x-noname@users.noreply.github.com>
2023-02-22 21:09:21 +0100

    Delete _site directory

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Startup - Try Hack Me",
-        "excerpt":"Linux / 10.10.186.208 Startup Abusar de las vulnerabilidades tradicionales a través de medios no tradidcionales. Escaneo de puertos abiertos ❯ nmap -p- --open -T5 -v -n 10.10.186.208 -oG puertos 21/tcp open ftp 22/tcp open ssh 80/tcp open http Escaneo de servicios ❯ nmap -sC -sV -p 21,22,80 10.10.186.208 -oN servicios...","categories": ["tryhackme","reverse-shell","bash"],
-        "tags": ["tryhackme","linux"],
-        "url": "http://localhost:4000/thm-startup/",
-        "teaser":"http://localhost:4000/assets/images/thm-writeup-startup/spicy.png"}]
